45016cf1b01135ad49c9780f3300536586ee0d76
Karam Koujan <kkoujan@e3r4p12.1337.ma>
2026-06-22 18:31:15 +0100

    start the project

---
+++ main.cpp
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+int main(int ac, char **av) {
+    if (ac != 3) {
+        std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
+        return 1;
+    }
+
+    try {
+        int port = std::atoi(av[1]);
+        if (port < 1024 || port > 65535) {
+            throw std::runtime_error("Invalid port range.");
+        }
+        std::string password = av[2];
+
+        Server server(port, password);
+        
+        server.start();
+    } 
+    catch (const std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
